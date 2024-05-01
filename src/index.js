75876567e8b5e3f3cd767f9781115cfffba78c4d
Owen Ou <o@owenou.com>
2024-04-30 21:34:55 -0700

    Use `--accept` to avoid wait time

--- src/index.js
+++ src/index.js
@@ -8,8 +8,6 @@ const { createActionAuth } = require("@octokit/auth-action");
 
 import { execShellCommand } from "./helpers"
 
-const UPTERM_VERSION = "v0.7.6"
-
 const sleep = (ms) => new Promise(resolve => setTimeout(resolve, ms));
 
 export async function run() {
@@ -21,7 +19,7 @@ export async function run() {
 
     core.debug("Installing dependencies")
     if (process.platform == "linux") {
-      await execShellCommand(`curl -sL https://github.com/owenthereal/upterm/releases/download/${UPTERM_VERSION}/upterm_linux_amd64.tar.gz | tar zxvf - -C /tmp upterm && sudo install /tmp/upterm /usr/local/bin/`)
+      await execShellCommand(`curl -sL https://github.com/owenthereal/upterm/releases/latest/download/upterm_linux_amd64.tar.gz | tar zxvf - -C /tmp upterm && sudo install /tmp/upterm /usr/local/bin/`)
       await execShellCommand("if ! command -v tmux &>/dev/null; then sudo apt-get -y install tmux; fi")
     } else {
       await execShellCommand("brew install owenthereal/upterm/upterm")
@@ -101,9 +99,7 @@ export async function run() {
     const uptermServer = core.getInput("upterm-server")
     const waitTimeoutMinutes = core.getInput("wait-timeout-minutes")
     core.info(`Creating a new session. Connecting to upterm server ${uptermServer}`)
-    await execShellCommand(`tmux new -d -s upterm-wrapper -x 132 -y 43 \"upterm host --server '${uptermServer}' ${authorizedKeysParameter} --force-command 'tmux attach -t upterm' -- tmux new -s upterm -x 132 -y 43\"`)
-    await sleep(2000)
-    await execShellCommand("tmux send-keys -t upterm-wrapper q C-m")
+    await execShellCommand(`tmux new -d -s upterm-wrapper -x 132 -y 43 \"upterm host --accept --server '${uptermServer}' ${authorizedKeysParameter} --force-command 'tmux attach -t upterm' -- tmux new -s upterm -x 132 -y 43\"`)
     // resize terminal for largest client by default
     await execShellCommand("tmux set -t upterm-wrapper window-size largest; tmux set -t upterm window-size largest")
     console.debug("Created new session successfully")
